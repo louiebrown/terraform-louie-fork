c4bea28c58d673f7ae99fe2928fd542cb1f80438
Louie Brown <louie.brown@kainos.com>
2025-01-27 13:23:38 +0000

    PA-01-enabling az cli auth and backend init

--- init.tf
+++ init.tf
@@ -5,8 +5,15 @@ terraform {
       version = "4.16.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "louie-terraform-rg"
+    storage_account_name = "louieterraformsa"
+    container_name = "terraform-state"
+    key = "terraform.tfstate"
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "d38fa016-777f-4647-bbc8-0aaf1933103c"
 }
 
-provider "azurerm" {
-  # Configuration options
-}
