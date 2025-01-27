f68032f26e51a620d4bc0e5ce8c28d913f9a62bc
Louie Brown <louie.brown@kainos.com>
2025-01-27 13:48:59 +0000

    PA-02-creatingPrivateVNETwith3Subnets

--- init.tf
+++ init.tf
@@ -18,6 +18,7 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   }
 
+# Public VNET
 resource "azurerm_virtual_network" "lb_public_vnet" {
   name                = "lb-public-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -29,10 +30,32 @@ resource "azurerm_virtual_network" "lb_public_vnet" {
   }
 }
 
+# Public Subnet x 3
 resource "azurerm_subnet" "lb_public_subnet" {
   count               = 3
   name                = "lb-public-subnet-${count.index}"
   resource_group_name = azurerm_virtual_network.lb_public_vnet.resource_group_name
   virtual_network_name = azurerm_virtual_network.lb_public_vnet.name
   address_prefixes    = ["10.0.${count.index}.0/24"]
+}
+
+# Private VNET
+resource "azurerm_virtual_network" "lb_private_vnet" {
+  name                = "lb-private-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = {
+    team = "louie-terraform"
+  }
+}
+
+# Private Subnet x 3
+resource "azurerm_subnet" "lb_private_subnet" {
+  count               = 3
+  name                = "lb-private-subnet-${count.index}"
+  resource_group_name = azurerm_virtual_network.lb_private_vnet.resource_group_name
+  virtual_network_name = azurerm_virtual_network.lb_private_vnet.name
+  address_prefixes    = ["10.1.${count.index}.0/24"]
 }
